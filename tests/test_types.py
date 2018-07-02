2792381c50225b09e5735487bb36c0f677c93c03
crohaco <crohaco@beproud.jp>
2018-07-02 23:09:34 +0900

    テスト用のファイル追加

---
+++ tests/test_types.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+class Test_alias(object):
+    def _callFUT(self, new_field, existing_field):
+        from d2a.types import alias
+        return alias(new_field, existing_field)
+
+    @pytest.fixture()
+    def charfield(self):
+        from django.db.models import CharField
+        return CharField
+
+    @pytest.fixture()
+    def newfield(self, charfield):
+        class NewField(charfield):
+            pass
+        return NewField
+
+    def test_new_field_added(self, newfield, charfield):
+        self._callFUT(newfield, charfield)
+        from d2a.types import mapping
+        assert newfield in mapping
