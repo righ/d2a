dbcdf7c8cbf6fcc2e6a17e2ee16882accae2014c
crohaco <crohaco@beproud.jp>
2018-07-03 18:40:11 +0900

    テストケース追加など

--- d2a/utils.py
+++ d2a/utils.py
@@ -2,9 +2,8 @@
 import re
 
 
-def get_camelcase(s):
+def get_camelcase(s, capitalize=False):
     rs = list(re.finditer(r'(?<=_)\w', s))
     for r in reversed(rs):
         s = s[:r.start()-1] + r.group(0).upper() + s[r.end():]
-    return s
-
+    return s[0:1].capitalize() + s[1:] if capitalize else s
